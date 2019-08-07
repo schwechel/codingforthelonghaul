95ca9cf8a0e1d40539b061d03275267490fb67cb
Derek Schwechel <d.schwechel@manitou-group.com>
2019-08-07 12:21:49 -0500

    Add unit test project

---
+++ CodingForTheLongHaul.Tests/OptionalArgumentsTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingForTheLongHaul.Tests
+{
+    [TestClass]
+    public class OptionalArgumentsTests
+    {
+        [TestMethod]
+        public void OptionalArguments_Tests()
+        {
+        }
+    }
+}
