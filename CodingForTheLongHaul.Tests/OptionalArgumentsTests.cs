8b339e9b16019246bd9db1b3d7a2718ce604c128
Derek Schwechel <d.schwechel@manitou-group.com>
2019-08-07 12:32:06 -0500

    Add Optional Arguments issue

--- CodingForTheLongHaul.Tests/OptionalArgumentsTests.cs
+++ CodingForTheLongHaul.Tests/OptionalArgumentsTests.cs
@@ -6,8 +6,33 @@ namespace CodingForTheLongHaul.Tests
     public class OptionalArgumentsTests
     {
         [TestMethod]
-        public void OptionalArguments_Tests()
+        public void OptionalArguments_2String_Test()
         {
+            var test = new OptionalArguments();
+
+            var result = test.LetsUseOptionalArguments("Go", "Brewers");
+
+            Assert.AreEqual("Go Brewers", result);
+        }
+
+        [TestMethod]
+        public void OptionalArguments_3String_Test()
+        {
+            var test = new OptionalArguments();
+
+            var result = test.LetsUseOptionalArguments("Let's", "Go", "Brewers");
+
+            Assert.AreEqual("Let's Go Brewers!!!", result);
+        }
+
+        [TestMethod]
+        public void OptionalArguments_4String_Test()
+        {
+            var test = new OptionalArguments();
+
+            var result = test.LetsUseOptionalArguments("Let's", "Go", "Brewers", "Not the Cubs");
+
+            Assert.AreEqual("Let's Go Brewers!!!", result);
         }
     }
 }
--- CodingForTheLongHaul/OptionalArguments.cs
+++ CodingForTheLongHaul/OptionalArguments.cs
@@ -6,5 +6,14 @@ namespace CodingForTheLongHaul
 {
     public class OptionalArguments
     {
+        public string LetsUseOptionalArguments(string one, string two)
+        {
+            return $"{one} {two}";
+        }
+
+        public string LetsUseOptionalArguments(string one, string two, string three = "", string four = "")
+        {
+            return $"{one} {two} {three}!!!";
+        }
     }
 }
