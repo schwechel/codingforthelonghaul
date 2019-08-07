cfe75c6311caeee608cca166cc6666eded26d4b0
Derek Schwechel <d.schwechel@manitou-group.com>
2019-08-07 12:19:01 -0500

    Add blank classes

---
+++ CodingForTheLongHaul/LargeRegions.cs
@@ -0,0 +1,27 @@
+﻿#region Imports
+
+using System;
+
+#endregion Imports
+
+namespace CodingForTheLongHaul
+{
+    public class LargeRegions
+    {
+        #region Private Members
+
+        #endregion Private Members
+
+        #region Constructors
+
+        #endregion Constructors
+
+        #region Public Methods
+
+        #endregion Public Methods
+
+        #region Private Methods
+
+        #endregion Private Methods
+    }
+}
---
+++ CodingForTheLongHaul/OptionalArguments.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingForTheLongHaul
+{
+    public class OptionalArguments
+    {
+    }
+}
